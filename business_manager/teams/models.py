87790047f0b55ca25c8d8c5697fa8c23b4e23d4f
bissaliev <bissaliev21@gmail.com>
2025-06-05 18:03:01 +0400

    Добавил модель Team

--- business_manager/teams/models.py
+++ business_manager/teams/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    """Модель команды"""
+
+    name = models.CharField("название команды", max_length=250, unique=True)
+    slug = models.SlugField("слаг", max_length=250, unique=True)
+    created_at = models.DateTimeField("Дата и время создания", auto_now_add=True, db_index=True)
+
+    class Meta:
+        verbose_name = "Команда"
+        verbose_name_plural = "Команды"
+        ordering = ["name", "-created_at"]
+
+    def __str__(self):
+        return f"{self.name} | Дата создания {self.created_at:%Y-%m-%d}"
