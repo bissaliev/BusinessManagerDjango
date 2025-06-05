258a0090d6fa5e24ced6bc927e48866056d54e34
bissaliev <bissaliev21@gmail.com>
2025-06-05 18:06:12 +0400

    Подключил маршруты приложения teams в главный урл

--- business_manager/business_manager/urls.py
+++ business_manager/business_manager/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("users/", include("users.urls", namespace="users")),
+    path("teams/", include("teams.urls", namespace="teams")),
 ]
