a81789556c3c9fd078c3a982b81e11bcd61f388e
bissaliev <bissaliev21@gmail.com>
2025-06-05 15:26:59 +0400

    Добавил модель User в админку

--- business_manager/users/admin.py
+++ business_manager/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_staff", "is_superuser", "role")
+    ordering = ("email",)
+    search_fields = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Персональная информация", {"fields": ("first_name", "last_name", "role")}),
+        ("Права доступа", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Важные даты", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
