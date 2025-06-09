9561794a6370dcaf11712aeef1f248eb2870d354
bissaliev <bissaliev21@gmail.com>
2025-06-09 16:21:34 +0400

    Добавил классы представления TeamDetailView, AddUserToTeamView, DeleteUserToTeamView

--- business_manager/teams/urls.py
+++ business_manager/teams/urls.py
@@ -6,8 +6,11 @@ app_name = "teams"
 
 
 urlpatterns = [
-    path("/", views.TeamListView.as_view(), name="team_list"),
+    path("", views.TeamListView.as_view(), name="team_list"),
     path("create/", views.TeamCreateView.as_view(), name="team_create"),
+    path("<int:pk>/", views.TeamDetailView.as_view(), name="team_detail"),
     path("<int:pk>/edit/", views.TeamEditView.as_view(), name="team_edit"),
     path("<int:pk>/delete/", views.TeamDeleteView.as_view(), name="team_delete"),
+    path("<int:pk>/add_user/", views.AddUserToTeamView.as_view(), name="add_user"),
+    path("<int:pk>/delete_user/<int:user_id>/", views.DeleteUserToTeamView.as_view(), name="delete_user"),
 ]
--- business_manager/teams/views.py
+++ business_manager/teams/views.py
@@ -1,12 +1,16 @@
 from django.core.paginator import Paginator
+from django.db.models import Count, F, Value
+from django.db.models.functions import Concat
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 
-from teams.forms import TeamForm
-from teams.models import Team
+from teams.forms import AddUserToTeamForm, TeamForm
+from teams.models import Team, TeamMembership
 
 
 class TeamCreateView(View):
+    """Создание команды"""
+
     form_class = TeamForm
     template_name = "teams/team_form.html"
 
@@ -23,11 +27,13 @@ class TeamCreateView(View):
 
 
 class TeamListView(View):
+    """Список команд"""
+
     template_name = "teams/team_list.html"
     model = Team
 
     def get(self, request):
-        teams = self.model.objects.all()
+        teams = self.model.objects.annotate(count_members=Count("members"))
         page_number = request.GET.get("page")
         paginator = Paginator(teams, 10)
         obj_list = paginator.get_page(page_number)
@@ -35,6 +41,8 @@ class TeamListView(View):
 
 
 class TeamEditView(View):
+    """Редактирование данных команды"""
+
     form_class = TeamForm
     template_name = "teams/team_form.html"
 
@@ -52,7 +60,27 @@ class TeamEditView(View):
         return render(request, self.template_name, {"form": form})
 
 
+class TeamDetailView(View):
+    """Детальная информация о команде"""
+
+    template_name = "teams/team_detail.html"
+
+    def get(self, request, pk):
+        team = get_object_or_404(Team, pk=pk)
+        employees = team.members.all()
+        employees = (
+            TeamMembership.objects.select_related("user")
+            .filter(team_id=team.id)
+            .annotate(
+                full_name=Concat(F("user__first_name"), Value(" "), F("user__last_name")),
+            )
+        )
+        return render(request, self.template_name, {"team": team, "employees": employees})
+
+
 class TeamDeleteView(View):
+    """Удаление команды"""
+
     template_name = "teams/team_confirm_delete_form.html"
 
     def get(self, request, pk: int):
@@ -63,3 +91,43 @@ class TeamDeleteView(View):
         team = get_object_or_404(Team, pk=pk)
         team.delete()
         return redirect("teams:team_list")
+
+
+class AddUserToTeamView(View):
+    """Добавление сотрудника в команду"""
+
+    template_name = "teams/add_user_to_team_form.html"
+    form_class = AddUserToTeamForm
+
+    def get(self, request, pk: int):
+        team = get_object_or_404(Team, pk=pk)
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form, "team": team})
+
+    def post(self, request, pk: int):
+        try:
+            team = get_object_or_404(Team, pk=pk)
+            form = self.form_class(request.POST)
+            if not form.is_valid():
+                raise ValueError("Invalid data")
+            if TeamMembership.objects.filter(user=form.cleaned_data["user"]).exists():
+                form.add_error("", "Пользователь уже зарегистрирован в команде")
+                raise ValueError("Пользователь уже зарегистрирован в команде")
+            form.save(commit=False)
+            form.instance.team = team
+            form.save()
+            return redirect("teams:team_detail", team.pk)
+        except ValueError:
+            return render(request, self.template_name, {"form": form, "team": team})
+
+
+class DeleteUserToTeamView(View):
+    """Удаление сотрудника из команды"""
+
+    form_class = AddUserToTeamForm
+
+    def post(self, request, pk: int, user_id: int):
+        emp = get_object_or_404(TeamMembership, team_id=pk, user_id=user_id)
+        team_id = emp.team_id
+        emp.delete()
+        return redirect("teams:team_detail", team_id)
