d5c809c47158f7d9d0f79ec29775834ed00451e8
bissaliev <bissaliev21@gmail.com>
2025-06-09 16:18:18 +0400

    Добавил  модель TeamMembership в админку

--- business_manager/teams/admin.py
+++ business_manager/teams/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from teams.models import Team
+from teams.models import Team, TeamMembership
+
+admin.site.register(TeamMembership)
 
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "created_at"]
+    list_display = ["id", "name", "created_at", "creator"]
     list_display_links = ["name"]
