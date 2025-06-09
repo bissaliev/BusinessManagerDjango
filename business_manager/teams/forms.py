7312cd0a98b8021e419a617f53aab88bc64d2944
bissaliev <bissaliev21@gmail.com>
2025-06-09 16:20:18 +0400

    Добавил форму AddUserToTeamForm для создания связи команду-сотрудник

--- business_manager/teams/forms.py
+++ business_manager/teams/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from teams.models import Team
+from teams.models import Team, TeamMembership
 
 
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
         fields = ("name", "slug")
+
+
+class AddUserToTeamForm(forms.ModelForm):
+    class Meta:
+        model = TeamMembership
+        fields = ("user", "role")
